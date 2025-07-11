f2e84c715e78be1e923fd71a608ae246ebe4c2f5
natali-fernandes <nahcrisfernandes@gmail.com>
2025-07-11 16:28:17 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg" />
+      <div className="login-box">
+        <h1>TEACHER MOMMY IA</h1>
+        <input type="email" placeholder="E-MAIL" />
+        <input type="password" placeholder="SENHA" />
+        <button>ENTRAR</button>
+        <p className="footer">Com amor, Teacher Mommy 💗</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
